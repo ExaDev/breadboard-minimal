853629984382b53f60966552a79de1001e5bf8ea
Joseph Mearman <joseph@mearman.co.uk>
2023-12-25 15:03:27 +0000

    Add Breadboard functionality to index.ts
    This commit adds the necessary code changes to incorporate the `@google-labs/breadboard` library into the `index.ts` file. It sets up a new `Board`, creates an input and output, and wires them together. Additionally, it includes a loop that runs the board and logs the results using a `LogProbe`.

--- index.ts
+++ index.ts
@@ -1,11 +1,36 @@
-import * as http from 'http';
-const server = http.createServer((_req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello, World!\n');
-});
-const port = 3000;
-const hostname = "0.0.0.0";
-server.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
+import { Board, LogProbe } from "@google-labs/breadboard";
+
+const board = new Board();
+
+const input = board.input();
+const output = board.output();
+input.wire("*", output);
+
+board.input().wire("*", board.output());
+board.input().wire("*", board.output());
+
+
+for await (const runResult of board.run({
+	probe: new LogProbe({
+		log: (...args: unknown[]) => {
+			console.debug();
+			console.debug("~".repeat(5) + " LogProbe " + "~".repeat(5));
+			console.debug(...args);
+			console.debug("~".repeat(20));
+		}
+	})
+})) {
+	console.log("=".repeat(80));
+	console.log(`node:\t${runResult.node.id}`);
+	console.log(`type:\t${runResult.type}`);
+	console.log();
+
+	if (runResult.type === "input") {
+		const inputs = {
+			timestamp: new Date().toISOString(),
+		};
+		runResult.inputs = inputs;
+	} else if (runResult.type === "output") {
+		console.log(runResult.outputs);
+	}
+}
